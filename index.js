526f7cc8db82878a36645a1be8307af1a3fe5010
siriroh kittiwuttikul <s55160304@gmail.com>
2025-03-13 15:31:54 +0700

    ทำ Hot reload เสร็จแล้ว

--- index.js
+++ index.js
@@ -38,9 +38,23 @@ app.get('/testddd',(req,res) => {
   res.json('testtest');
 })
 
-app.get('/tortestgit',(req,res));
-app.get('/tortestmerge',(req,res));
-app.get('/torapigateway',(req,res));
+app.get('/testdoc',(req,res) => {
+  console.log('testdoc');
+})
+
+app.get('/test',(req,res) => {
+  console.log('test ja');
+})
+
+app.get('/armtest',(req,res) => {
+  res.json("arm Test")
+})
+
+app.get('/tortest',(req,res) => {
+  res.json('docker hot reload');
+})
+
+
 
 app.listen(port, async() => {
   await initMySQL();
